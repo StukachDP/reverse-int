9396d27fbb6bcf8e1312714dcc9cc444ebab9538
StukachDP <stukachdima06112000@gmail.com>
2020-10-16 20:57:18 +0300

    Solution of ReverseInt task.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let result = 0;
+    let digitOfNumber;
+    n = Math.abs(n);
+    while (n > 0) {
+        digitOfNumber = n % 10;
+        n = parseInt(n / 10);
+        result = result * 10 + digitOfNumber;
+    }
+    return result;
+};
